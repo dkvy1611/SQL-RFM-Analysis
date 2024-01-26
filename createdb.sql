fe1119052e13fa7a335fa11948040f14a4caa30b
Rachel <136768953+racheldkv@users.noreply.github.com>
2024-01-26 18:00:12 +0700

    Create createdb.sql

---
+++ createdb.sql
@@ -0,0 +1,33 @@
+create table SALES_DATASET_RFM_PRJ
+(
+  ordernumber VARCHAR,
+  quantityordered VARCHAR,
+  priceeach        VARCHAR,
+  orderlinenumber  VARCHAR,
+  sales            VARCHAR,
+  orderdate        VARCHAR,
+  status           VARCHAR,
+  productline      VARCHAR,
+  msrp             VARCHAR,
+  productcode      VARCHAR,
+  customername     VARCHAR,
+  phone            VARCHAR,
+  addressline1     VARCHAR,
+  addressline2     VARCHAR,
+  city             VARCHAR,
+  state            VARCHAR,
+  postalcode       VARCHAR,
+  country          VARCHAR,
+  territory        VARCHAR,
+  contactfullname  VARCHAR,
+  dealsize         VARCHAR
+);
+
+
+CREATE TABLE segment_score
+(
+    segment Varchar,
+    scores Varchar)
+
+-- Lần lượt import 2 file sales_data_clean_rfm và segment_score1
+
