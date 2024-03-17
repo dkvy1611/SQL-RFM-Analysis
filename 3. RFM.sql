350eb50514f545ebb644b470924ccc7b3dc87f3f
Khánh Vy <136768953+dkvy1611@users.noreply.github.com>
2024-03-17 21:03:14 +0700

    Update 3. RFM.sql

--- 3. RFM.sql
+++ 3. RFM.sql
@@ -17,12 +17,11 @@ ORDER BY PRODUCTLINE,
 --2) Tháng có bán nhất mỗi năm?
 WITH cte2 AS
   (SELECT *,
-          rank() over(PARTITION BY year_id
-                      ORDER BY revenue DESC) AS rank
+          rank() over(PARTITION BY year_id ORDER BY revenue DESC) AS rank
    FROM
      (SELECT year_id,
              month_id,
-             sum(quantityordered*priceeach) AS REVENUE
+             sum(quantityordered*priceeach) AS revenue
       FROM public.sales_dataset_rfm_prj
       GROUP BY month_id,
                year_id) a)
@@ -34,7 +33,7 @@ WHERE rank =1;
 
 --3) Product line nào được bán nhiều ở tháng 11?
 SELECT month_id,
-       sum(quantityordered*priceeach) AS REVENUE,
+       SUM(quantityordered*priceeach) AS revenue,
        productline
 FROM public.sales_dataset_rfm_prj
 WHERE month_id=11
@@ -46,12 +45,11 @@ LIMIT 1;
 --4) Đâu là sản phẩm có doanh thu tốt nhất ở UK mỗi năm? Xếp hạng các các doanh thu đó theo từng năm.
 WITH cte4 AS
   (SELECT *,
-          rank() over(PARTITION BY YEAR_ID
-                      ORDER BY revenue DESC) AS rank
+          rank() over(PARTITION BY YEAR_ID ORDER BY revenue DESC) AS rank
    FROM
      (SELECT YEAR_ID,
              PRODUCTLINE,
-             sum(quantityordered*priceeach) AS REVENUE
+             sum(quantityordered*priceeach) AS revenue
       FROM public.sales_dataset_rfm_prj
       WHERE country = 'UK'
       GROUP BY YEAR_ID,
@@ -63,25 +61,22 @@ WHERE rank=1;
 
 --5) Ai là khách hàng tốt nhất, phân tích dựa vào RFM 
 WITH customer_rfm AS
-  (SELECT customername,
-          CURRENT_DATE - max(orderdate) AS R,
-          COUNT(DISTINCT ordernumber) AS F,
-          SUM(quantityordered*priceeach) AS M
-   FROM public.sales_dataset_rfm_prj
-   GROUP BY customername),
+         (SELECT customername,
+                 CURRENT_DATE - max(orderdate) AS R,
+                 COUNT(DISTINCT ordernumber) AS F,
+                 SUM(quantityordered*priceeach) AS M
+          FROM public.sales_dataset_rfm_prj
+          GROUP BY customername),
      rfm_score AS
-  (SELECT customername,
-          ntile(5) OVER (
-                         ORDER BY r DESC) AS r_score,
-                        ntile(5) OVER (
-                                       ORDER BY f) AS f_score,
-                                      ntile(5) OVER (
-                                                     ORDER BY m) AS m_score
-   FROM customer_rfm),
+         (SELECT customername,
+                 ntile(5) OVER (ORDER BY r DESC) AS r_score,
+                 ntile(5) OVER (ORDER BY f) AS f_score,
+                 ntile(5) OVER (ORDER BY m) AS m_score
+          FROM customer_rfm),
      rfm_final AS
-  (SELECT customername,
-          cast(r_score AS varchar)|| cast(f_score AS varchar)|| cast(m_score AS varchar)AS rfm
-   FROM rfm_score)
+         (SELECT customername,
+                 cast(r_score AS varchar)|| cast(f_score AS varchar)|| cast(m_score AS varchar)AS rfm
+          FROM rfm_score)
 SELECT *
 FROM
   (SELECT a.customername,
